bddefc5aca874c7631bdcefba8beb31c38a8b1d9
Elise Yohm <elisery@hotmail.com>
2018-08-31 18:36:52 -0700

    Update config

--- config/application.rb
+++ config/application.rb
@@ -53,12 +53,12 @@ module FraveOne
     #     # allowed.
     #   end
     # end
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+    #   end
+    # end
 
   end
 end
