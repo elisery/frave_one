7e7cb58f8a85caece2c844346bb3577c622d347f
Elise Yohm <elisery@hotmail.com>
2018-08-31 19:03:23 -0700

    Update config for CORS

--- config/routes.rb
+++ config/routes.rb
@@ -42,14 +42,7 @@ Rails.application.routes.draw do
     resources :reward_items, only:[:new, :create, :edit, :update]
   end
 
-  match "*path", :to => proc {|env| [200, {
-  'Access-Control-Allow-Origin' => '*',
-  'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Credentials' => 'true',
-  'Access-Control-Request-Method' => '*',
-  'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-'Content-Type' => 'text/plain'
-
- }, ["CORS Preflight"]] }, :via => [:options]
+  # global options responder -> makes sure OPTION request for CORS endpoints work
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
 
 end
