d08163bfeaca3224f4e7ee482a05688583b4ed46
Elise Yohm <elisery@hotmail.com>
2018-08-31 18:28:18 -0700

    Update application for cors

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module FraveOne
     #     # allowed.
     #   end
     # end
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
