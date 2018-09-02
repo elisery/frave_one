e4f81b6166c1f186c1818cb5fadef3bdf84a38e2
Elise Yohm <elisery@hotmail.com>
2018-09-01 17:49:13 -0700

    Fix config

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module FraveOne
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'frave-react.herokuapp.com', 'frave.herokuapp.com',
+        origins 'frave-react.herokuapp.com', 'frave.herokuapp.com'
         # 'origin' option specifies the domains that are allowed to make cross
         # origin requests to our Rails server
         resource(
